d716b286574449046f8dbbaf9573c4ef1ef62680
Leandro <leandrofelipecunha@hotmail.com>
2020-12-16 18:12:19 -0300

    ex015

--- guppe/exercicios/seção_04/ex014.py
+++ guppe/exercicios/seção_04/ex014.py
@@ -1,3 +1,5 @@
 # Ler um angulo em Graus e converter em Radianos
 ang = float(input('Digite um ângulo em Graus: '))
 rad = ang * 3.14 / 180
+
+print(f'O ângulo convertido em Radianos é: {rad}')
---
+++ guppe/exercicios/seção_04/ex015.py
@@ -0,0 +1,5 @@
+# Ler um angulo em rad e converter em graus
+ang = float(input('Digite um ângulo em Radianos: '))
+graus = ang * 180 / 3.14
+
+print(f'O ângulo convertido em Graus é: {graus}')
