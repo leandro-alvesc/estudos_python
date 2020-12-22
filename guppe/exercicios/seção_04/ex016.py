dd9c78bd1085634cd683ab592dded69cc3540b2f
Leandro <leandrofelipecunha@hotmail.com>
2020-12-22 16:57:28 -0300

    ex 16 e 17

---
+++ guppe/exercicios/seção_04/ex016.py
@@ -0,0 +1,6 @@
+# Ler um valor em polegadas e converter para cm
+val = float(input('Digite o valor em Polegadas: '))
+
+cm = val * 2.54
+
+print(f'O valor convertido é {cm}cm')
---
+++ guppe/exercicios/seção_04/ex017.py
@@ -0,0 +1,6 @@
+# Ler um valor em cm e converter para polegadas
+val = float(input('Digite o valor em Centímetros: '))
+
+pol = val / 2.54
+
+print(f'O valor convertido é {pol}cm')
